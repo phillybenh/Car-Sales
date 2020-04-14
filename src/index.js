3cc4aeade16626bb2a4a214ad2c7b87a0c1a1e89
Ben Haus <ben@little-haus.com>
2020-04-14 16:44:34 -0400

    index.js set up with store and Provider

--- src/index.js
+++ src/index.js
@@ -1,9 +1,21 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './App';
+import React from "react";
+import ReactDOM from "react-dom";
+import { createStore } from "redux";
+import { Provider } from "react-redux";
 
-import 'bulma/css/bulma.css';
-import './styles.scss';
+import App from "./App";
+import { reducer } from "./reducers/reducer";
 
-const rootElement = document.getElementById('root');
-ReactDOM.render(<App />, rootElement);
+import "bulma/css/bulma.css";
+import "./styles.scss";
+
+const store = createStore(reducer);
+console.log('current state, from index', store.getState())
+
+const rootElement = document.getElementById("root");
+ReactDOM.render(
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  rootElement
+);
